d43ddabcd899a44c0b28f7b1b39c435574ffb90b
smartdeng <zero.dengc@outlook.com>
2019-07-31 16:39:59 +0800

    delete OS/Java file

--- prj_final/src/cn/dc/service/BookService.java
+++
@@ -1,6 +0,0 @@
-package cn.dc.service;
-import cn.dc.service.impl.BookServiceImpl;
-
-public class BookService implements BookServiceImpl{
-    //声明购买图书的流程
-}
--- prj_final/src/cn/dc/service/impl/BookServiceImpl.java
+++
@@ -1,4 +0,0 @@
-package cn.dc.service.impl;
-
-public interface BookServiceImpl {
-}
